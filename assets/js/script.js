134475368fe72195873a76c0d8b4a756439073ee
Kshitij <kshtjkmr35@gmail.com>
2018-02-10 18:43:55 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
 // @ts-check
 
-var count = 0;
-var THqueue = [];
-var sEle = [];
 
-var hum = document.querySelector('humidity');
-var tem = document.querySelector('temperature');
+var s11 = document.querySelector('s1');
+var s12 = document.querySelector('s2');
+var s13 = document.querySelector('s3');
 
-var t_max = document.querySelector('tmax');
-var t_min = document.querySelector('tmin');
-var t_avg = document.querySelector('tavg');
+var s14 = document.querySelector('s4');
+var s15 = document.querySelector('s5');
+var s16 = document.querySelector('s6');
 
-var h_max = document.querySelector('hmax');
-var h_min = document.querySelector('hmin');
-var h_avg = document.querySelector('havg');
+var s17 = document.querySelector('s7');
+var s18 = document.querySelector('s8');
+//var s19 = document.querySelector('s9');
 
-//var c = document.querySelector('clock');
+var c = document.querySelector('clock');
+var stats = document.querySelector('st')
 
-/*
+
 var pad = function (x) {
     return x < 10 ? '0' + x : x;
 }
@@ -31,72 +30,25 @@ var ShowClock = function () {
     c.innerHTML = [h, m, s].join(':');
 }
 
-setInterval(ShowClock, 1000);*/
-
-var tmax = 0,
-    tmin = 99,
-    tavg, ttotal = 0;
-var hmax = 0,
-    hmin = 99,
-    havg, htotal = 0;
+setInterval(ShowClock, 1000);
 
 var myRef = new Firebase('https://testiot-2018.firebaseio.com/');
 
-myRef.on('child_changed', function (snapshot) {
+myRef.on('child_changed', function(snapshot){
     data = snapshot.val();
+    s11.innerHTML = data.SensorA;
+    s12.innerHTML = data.SensorB;
+    s13.innerHTML = data.SensorC;
 
-    var h, t;
-    THqueue.push([data.Humidity, data.Temperature]);
-    h = THqueue[count][0];
-    t = THqueue[count][1];
+    s14.innerHTML = data.SensorD;
+    s15.innerHTML = data.SensorE;
+    s16.innerHTML = data.SensorF;
+    
+    s17.innerHTML = data.SensorG;
+    s18.innerHTML = data.SensorH;
 
-    ttotal = ttotal + t;
-    htotal = htotal + h;
-
-    count = count + 1;
-
-    tavg = Number(ttotal / count).toFixed(2);
-    havg = Number(htotal / count).toFixed(2);
-
-    if (t > tmax) {
-        tmax = t;
-    }
-    if (t < tmin) {
-        tmin = t;
-    }
-    if (h > hmax) {
-        hmax = h;
-    }
-    if (h < hmin) {
-        hmin = h;
-    }
-
-    hum.innerHTML = h;
-    tem.innerHTML = t;
-
-    t_max.innerHTML = tmax;
-    t_min.innerHTML = tmin;
-    t_avg.innerHTML = tavg;
-
-    h_max.innerHTML = hmax;
-    h_min.innerHTML = hmin;
-    h_avg.innerHTML = havg;
-
-    if (count == 11) {
-        sEle[0] = THqueue.shift();
-        ttotal -= sEle[0][1];
-        htotal -= sEle[0][0];
-
-        count = count - 1;
-    }
-
-    /*var i, j;
-    var myTable = document.getElementById('myTable');
-    for (i = 1; i <= THqueue.length; i++) {
-        myTable.rows[i].cells[0].innerHTML = i;
-        myTable.rows[i].cells[1].innerHTML = THqueue[i - 1][0];
-        myTable.rows[i].cells[2].innerHTML = THqueue[i - 1][1];
-    }*/
-
-
+	stats.innerHTML = data.Status;
+    
 });
+
+
